2bd3aea8b9e0aef37e6c29d6067f6ee2bcfedf49
Bryan Luu <bluu@proofgov.com>
2021-12-09 15:20:00 -0800

    :art: cleanup unused code for Day 9

--- Day9/basin.rb
+++ Day9/basin.rb
@@ -2,20 +2,13 @@ class Basin
   attr_reader :size
 
   def initialize(position)
-    @visited = Hash.new(false)
     @included = Hash.new(false)
-    @visited[key_for(position)] = true
-    @included[key_for(position)] = true
-    @size = 1
+    @size = 0
     @root = position
   end
 
   def measure
-    puts "--- #{key_for(@root)}: #{@root.height} ---"
-    @root.neighbors.each do |target|
-      build_basin(@root, target)
-    end
-    puts "basin: #{@included.keys.inspect}"
+    build_basin(@root)
   end
 
   private
@@ -24,17 +17,16 @@ class Basin
     [position.row, position.column]
   end
 
-  def build_basin(src, target, level: 0)
+  def build_basin(target, level: 0)
     # peform a DFS on the target node, keeping track of the source node
     return if @included[key_for(target)]
     return if target.height == 9
 
     @included[key_for(target)] = true
     @size += 1
-    puts '   ' * level + "add #{key_for(target).inspect}: #{target.height} to basin"
 
     target.neighbors.each do |neighbor|
-      build_basin(target, neighbor, level: level + 1)
+      build_basin(neighbor, level: level + 1)
     end
   end
 end
